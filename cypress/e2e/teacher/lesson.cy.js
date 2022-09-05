c4abe1c326892756131ce1feb4073cb49125702b
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-09-05 02:56:56 -0500

    saveTemplate and saveSolution test complete :hatched_chick:

--- cypress/e2e/teacher/lesson.cy.js
+++ cypress/e2e/teacher/lesson.cy.js
@@ -13,43 +13,41 @@ describe('lesson screen', () => {
             })
     })
 
-    //Fallos: Cuando cambio de tab, se borra el contenido. 
     it("Save template", function () {
         cy.contains('Template').click()
-        cy.get('.view-line').type('print("Hello!")')
+        cy.get('.view-line').eq(0).type('print("Hello!")')
 
         cy.contains('Solution').click()
-        cy.get('.view-line').type('print("Hello world!")')
+        cy.get('.view-line').eq(1).type('print("Hello world!")')
 
         cy.contains('Save').click()
         cy.wait(1000)
 
+        //Reload 1
         cy.visit('http://localhost:3000/course/1/lesson/' + this.stepId)
 
         cy.contains('Template').click()
         cy.wait(2000)
-        cy.get('.view-line').should('have.text', 'print("Hello!")')
+        cy.get('.view-line').eq(0).should('have.text', 'print("Hello!")')
         .type(' print("Hello2!")')
 
-        cy.contains('Save').click()
-        cy.wait(1000)
-
         cy.contains('Solution').click()
         cy.wait(2000)
-        cy.get('.view-line').contains('print("Hello world!")')//should don't accept white spaces
-        cy.get('.view-line').type(' print("Hello World2!")')
+        cy.get('.view-line').eq(1).contains('print("Hello world!")').should('exist')//should don't accept white spaces
+        cy.get('.view-line').eq(1).type(' print("Hello World2!")')
 
         cy.contains('Save').click()
         cy.wait(1000)
 
+        //Reload 2
         cy.visit('http://localhost:3000/course/1/lesson/' + this.stepId)
 
         cy.contains('Template').click()
         cy.wait(2000)
-        cy.get('.view-line').contains('print("Hello!") print("Hello2!")')//should don't accept white spaces
+        cy.get('.view-line').eq(0).contains('print("Hello!") print("Hello2!")').should('exist')//should don't accept white spaces
 
         cy.contains('Solution').click()
         cy.wait(2000)
-        cy.get('.view-line').contains('print("Hello world!") print("Hello world2!")')//should don't accept white spaces
+        cy.get('.view-line').eq(1).contains('print("Hello world!") print("Hello World2!")').should('exist')//should don't accept white spaces
     })
 })
