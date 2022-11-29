5fca04684e61a1dd415333a53522b7ebd385d2a9
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2022-11-29 13:34:47 -0500

    ESlint warning suppressor

--- next.config.js
+++ next.config.js
@@ -5,4 +5,10 @@ const nextConfig = {
 }
 const removeImports = require("next-remove-imports")();
 
-module.exports = removeImports({});
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
